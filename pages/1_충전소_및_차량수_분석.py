726d8fea9ddb8f2bc1988b99ee9efd226e049f39
imngooh <goodjw0125@gmail.com>
2022-10-23 20:42:43 +0900

    folium_static 활용 + 데이터 각각 읽어오기

--- pages/1_충전소_및_차량수_분석.py
+++ pages/1_충전소_및_차량수_분석.py
@@ -17,52 +17,10 @@ st.set_page_config(
 )
 st.title('충전기 수와 전기차 수 비교')
 
-
 # 데이터 읽어오기
-df = pd.read_parquet('https://github.com/cryptnomy/likelion-ai-s7-mid/blob/master/data/hypo_2.parqeut.gzip?raw=true')
-
-
-ee_car = pd.read_csv('https://raw.githubusercontent.com/cryptnomy/likelion-ai-s7-mid/master/data/seoul-any-gu-ev.csv', encoding = 'cp949')
-ee_car['시군구별'] = ee_car['시군구별'].str.replace('중 구', '중구')
-ee_car = ee_car[ee_car['연월별'] == '2021-12-31'][['시군구별','계']]
-ee_car_mount = pd.DataFrame(ee_car.groupby('시군구별')['계'].sum()).reset_index()
-ee_car_mount = ee_car_mount.rename(columns = {'시군구별' : '자치구'})
-ee_station = df.groupby('자치구')['충전소명'].nunique().copy()
-ee_station = pd.DataFrame(ee_station).reset_index()
-ee_station = ee_station.rename(columns = {'충전소명' : '충전소수'})
-ee_per = pd.merge(ee_car_mount,ee_station,on='자치구')
-ee_per = ee_per.rename(columns = {"계" : "자치구별 전기차 대수"})
-gu_charger = df.groupby(['자치구','충전소명'])[['급속충전기(대)','완속충전기(대)']].mean().groupby('자치구').sum().reset_index()
-gu_charger['총 충전기수'] = gu_charger['급속충전기(대)'] + gu_charger['완속충전기(대)']
-ee_per = pd.merge(ee_per , gu_charger)
-ee_per['충전기 당 전기차'] = ee_per['자치구별 전기차 대수'] / ee_per['총 충전기수']
-
-
-oil = pd.read_csv('https://raw.githubusercontent.com/cryptnomy/likelion-ai-s7-mid/master/data/20221020101743.csv')
-oil = oil[~oil['자치구별(2)'].str.contains('소계')]
-oil = oil[oil['판매소별(2)'].str.startswith('주유소')]
-oil = oil.drop(["자치구별(1)", "판매소별(1)", "2011","2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"], axis=1)
-oil = oil.rename(columns = {"자치구별(2)" : "자치구", "판매소별(2)" : "판매소 유형", "2021" : "2021주유소 현황"})
-oil_machine = pd.read_csv('https://raw.githubusercontent.com/cryptnomy/likelion-ai-s7-mid/master/data/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C_%EC%84%B1%EB%B6%81%EA%B5%AC_%EC%A3%BC%EC%9C%A0%EC%86%8C_%ED%98%84%ED%99%A9_20210101.csv', encoding = 'cp949')
-mean_oil = oil_machine['주유기수'].mean()
-oil['주유기수'] = oil['2021주유소 현황'].astype(int).map(lambda x : round(x * mean_oil))
-
-
-cc=pd.read_csv("https://raw.githubusercontent.com/cryptnomy/likelion-ai-s7-mid/master/data/20221020162020.csv")
-cc = cc[~cc['자치구별(2)'].str.startswith("소계")]
-cc = cc[~cc['자치구별(2)'].str.startswith("자치구별(2)")]
-cc = cc.drop(["자치구별(1)", "2021.1", "2021.2" , "2021.3" , "2021.4", "2021.5", "2021.6", "2021.7", "2021.8", "2021.9", "2021.10", "2021.11", "2021.12", "2021.13", "2021.14", "2021.15", "2021.16", "2021.17", "2021.18", "2021.19", "2021.20", "2021.21", "2021.22", "2021.23"], axis=1)
-cc = cc.rename(columns = {"자치구별(2)" : "자치구", "2021" : "2021등록된자동차"})
-oil_cc = pd.merge(oil,cc, how="left")
-oil_cc["주유기 당 자동차"] = oil_cc["2021등록된자동차"].astype(int)/oil_cc["주유기수"]
-oil_cc["주유기 당 자동차"] = round(oil_cc["주유기 당 자동차"], 2)
-
-
-fuel_ee = pd.merge(oil_cc[['자치구','주유기 당 자동차']], ee_per[['자치구','충전기 당 전기차']], on ='자치구')
-
-
-
-
+ee_per = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/ee_per.csv')
+oil_cc = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/oil_cc.csv')
+fuel_ee = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/fuel_ee.csv')
 
 
 
--- 시비걸지마시조.py
+++ 시비걸지마시조.py
@@ -5,6 +5,7 @@ import koreanize_matplotlib
 import matplotlib.pyplot as plt
 import plotly.express as px
 import folium
+from streamlit_folium import folium_static
 from streamlit_folium import st_folium
 import streamlit as st
 import requests
@@ -16,64 +17,16 @@ st.set_page_config(
     initial_sidebar_state= 'expanded'
 )
 
+# 데이터 로드
+df_gu_map = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/df_gu_map.csv')
+df_map = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/df_map.csv')
+ee_per = pd.read_csv('https://raw.githubusercontent.com/imngooh/ais7_demo/main/ee_per.csv')
+
 
 # 지도 생성
 final_map = folium.Map(location = [37.5759,126.9768], zoom_start = 11)
 
-# 지도 위한 데이터
-df = pd.read_parquet('https://github.com/cryptnomy/likelion-ai-s7-mid/blob/master/data/hypo_2.parqeut.gzip?raw=true')
-
-df_map = pd.DataFrame(df['충전소명'].unique()).rename(columns = {0 : '충전소명'})
-df_map = pd.merge(df_map, df.groupby(['충전소명','위도','경도','급속충전기(대)','완속충전기(대)']).mean().reset_index()[['위도','경도','충전소명','급속충전기(대)','완속충전기(대)']], on ='충전소명')
-df_map = pd.merge(df.groupby(['자치구','충전소명']).sum().reset_index()[['자치구','충전소명']], df_map)
-df_gu_map= pd.merge(df_map.groupby('자치구')[['급속충전기(대)','완속충전기(대)']].sum().reset_index(), df_map.groupby('자치구')['충전소명'].count().reset_index())
-gu_loc = """강남구 37.5172 127.0473
-강동구 37.5301 127.1238
-강북구 37.6398 127.0255
-강서구 37.5510 126.8495
-관악구 37.4781 126.9515
-광진구 37.5384 127.0822
-구로구 37.4955 126.8876
-금천구 37.4519 126.9020
-노원구 37.6543 127.0575
-도봉구 37.6688 127.0471
-동대문구 37.5742 127.0398
-동작구 37.5124 126.9393
-마포구 37.5634 126.9034
-서대문구 37.5793 126.9365
-서초구 37.4836 127.0327
-성동구 37.5634 127.0369
-성북구 37.5894 127.0167
-송파구 37.5117 127.1059
-양천구 37.5170 126.8666
-영등포구 37.5263 126.8963
-용산구 37.5323 126.9907
-은평구 37.6015 126.9304
-종로구 37.5735 126.9790
-중구 37.5641 126.9979
-중랑구 37.6063 127.0932"""
-gu_df = pd.DataFrame(gu_loc.split('\n'))[0].str.split(expand = True)
-gu_df.columns = ['자치구', '위도','경도']
-df_gu_map = pd.merge(df_gu_map, gu_df)
 geo_data = requests.get('https://raw.githubusercontent.com/cubensys/Korea_District/master/3_%EC%84%9C%EC%9A%B8%EC%8B%9C_%EC%9E%90%EC%B9%98%EA%B5%AC/%EC%84%9C%EC%9A%B8_%EC%9E%90%EC%B9%98%EA%B5%AC_%EA%B2%BD%EA%B3%84_2017.geojson').json()
-
-ee_car = pd.read_csv('https://raw.githubusercontent.com/cryptnomy/likelion-ai-s7-mid/master/data/seoul-any-gu-ev.csv', encoding = 'cp949')
-ee_car['시군구별'] = ee_car['시군구별'].str.replace('중 구', '중구')
-ee_car = ee_car[ee_car['연월별'] == '2021-12-31'][['시군구별','계']]
-ee_car_mount = pd.DataFrame(ee_car.groupby('시군구별')['계'].sum()).reset_index()
-ee_car_mount = ee_car_mount.rename(columns = {'시군구별' : '자치구'})
-ee_station = df.groupby('자치구')['충전소명'].nunique().copy()
-ee_station = pd.DataFrame(ee_station).reset_index()
-ee_station = ee_station.rename(columns = {'충전소명' : '충전소수'})
-ee_per = pd.merge(ee_car_mount,ee_station,on='자치구')
-ee_per = ee_per.rename(columns = {"계" : "자치구별 전기차 대수"})
-
-gu_charger = df.groupby(['자치구','충전소명'])[['급속충전기(대)','완속충전기(대)']].mean().groupby('자치구').sum().reset_index()
-gu_charger['총 충전기수'] = gu_charger['급속충전기(대)'] + gu_charger['완속충전기(대)']
-
-ee_per = pd.merge(ee_per , gu_charger)
-ee_per['충전기 당 전기차'] = ee_per['자치구별 전기차 대수'] / ee_per['총 충전기수']
-
 
 folium.Choropleth(
     geo_data = geo_data,
@@ -84,6 +37,7 @@ folium.Choropleth(
     legend_name = '충전소 수',
 ).add_to(final_map)
 from folium.plugins import MarkerCluster
+
 mc = MarkerCluster()
 
 for i, row in df_map.iterrows():
@@ -97,10 +51,8 @@ for i, row in df_map.iterrows():
               )
     )
 
+
 mc.add_to(final_map)    
- 
-
-
 
 
 
@@ -129,7 +81,7 @@ st.title('***전기차 충전소는 정말로 부족한가?***')
 
 '### 충전소는 진짜로 부족할까?'
 with st.expander('서울특별시 내 충전기 지도로 살펴보기!') :
-    st_map = st_folium(final_map)
+    st_map = folium_static(final_map)
     '배경 색상은 충전기 하나 당 담당하는 전기차 수를 의미합니다.'
 st.image('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/5575478b-24da-4767-98b2-4f9a77cc1053/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221022%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221022T171016Z&X-Amz-Expires=86400&X-Amz-Signature=ad0b565654a99571055aea8a10e3f337fb83aab3dbd22482adf30a9f9165093d&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22Untitled.png%22&x-id=GetObject')
 """
